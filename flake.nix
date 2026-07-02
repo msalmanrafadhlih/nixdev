d45acb74cc0bb9d2c0022378cfbfba8d29811160
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-03 03:07:29 +0700

    Update via Auto Commit - 2026-07-03 03:07
    Update file: flake.nix
    
    Changes:
    -        flutter    = inputs.flutter.devShells.${system}.default;
    -        nodejs     = inputs.nodejs.devShells.${system}.default;
    -        rust-basic = inputs.rust.devShells.${system}.default;
    -        bun        = inputs.bun.devShells.${system}.default;
    -        flutter     = flutter.devenvModules.default;
    -        nodejs      = nodejs.devenvModules.default;
    -        bun         = bun.devenvModules.default;
    -        rust-basic  = rust.devenvModules.default;
    -    nixpkgs-lib.url = "github:NixOS/nixpkgs/nixos-unstable?dir=lib";
    -
    -
    -    rust.url    = "path:./rust";
    -    flutter.url = "path:./flutter";
    -    bun.url     = "path:./bun";
    -    nodejs.url  = "path:./nodejs";

--- flake.nix
+++ flake.nix
@@ -11,30 +11,31 @@
 
       # nix develop github:msalmanrafadhlih/nix.templates#<template> --impure
       devShells = lib.genAttrs lib.systems.flakeExposed (system: {
-        flutter    = inputs.flutter.devShells.${system}.default;
-        nodejs     = inputs.nodejs.devShells.${system}.default;
-        rust-basic = inputs.rust.devShells.${system}.default;
-        bun        = inputs.bun.devShells.${system}.default;
+        flutter      = inputs.flutter.devShells.${system}.default;
+        nodejs       = inputs.nodejs.devShells.${system}.default;
+        rust-basic   = inputs.rust.devShells.${system}.default;
+        tauri-mobile = inputs.bun.devShells.${system}.default;
+        bun          = inputs.bun.devShells.${system}.default;
       });
 
       # inputs.<thisrepo>.devenvModules.<template>
       devenvModules = with inputs; {
-        flutter     = flutter.devenvModules.default;
-        nodejs      = nodejs.devenvModules.default;
-        bun         = bun.devenvModules.default;
-        rust-basic  = rust.devenvModules.default;
+        flutter      = flutter.devenvModules.default;
+        nodejs       = nodejs.devenvModules.default;
+        bun          = bun.devenvModules.default;
+        rust-basic   = rust.devenvModules.default;
+        tauri-mobile = tauri-mobile.devenvModules.default;
       };
     };
 
   inputs = {
-    nixpkgs-lib.url = "github:NixOS/nixpkgs/nixos-unstable?dir=lib";
-
+    nixpkgs-lib.url   = "github:NixOS/nixpkgs/nixos-unstable?dir=lib";
     # templates
     nix-templates.url = "github:nix-community/templates";
-
-    rust.url    = "path:./rust";
-    flutter.url = "path:./flutter";
-    bun.url     = "path:./bun";
-    nodejs.url  = "path:./nodejs";
+    rust.url          = "path:./rust";
+    flutter.url       = "path:./flutter";
+    bun.url           = "path:./bun";
+    nodejs.url        = "path:./nodejs";
+    tauri-mobile.url  = "path:./tauri-mobile";
   };
 }
